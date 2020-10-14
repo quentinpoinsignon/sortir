5d811e5f3b564d0b29424cad931b6b291de8504b
adeline8444 <68584539+adeline8444@users.noreply.github.com>
2020-10-14 10:18:03 +0200

    Fix security.yaml

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -50,6 +50,7 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator implements Passwo
         $credentials = [
             'username' => $request->request->get('username'),
             'password' => $request->request->get('password'),
+            'mail' => $request->request->get('emailAdress'),
             'csrf_token' => $request->request->get('_csrf_token'),
         ];
         $request->getSession()->set(
