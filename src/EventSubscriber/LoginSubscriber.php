145ba14ec59a9ef8de3e454869cca6fcb275ed0f
Kim <68189659+kimmaroe@users.noreply.github.com>
2020-10-21 15:06:23 +0200

    LoginSubscriber.php StateService.php

--- src/EventSubscriber/LoginSubscriber.php
+++ src/EventSubscriber/LoginSubscriber.php
@@ -9,12 +9,14 @@ use Doctrine\ORM\EntityManagerInterface as EntityManager;
 use Exception;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Event\AuthenticationEvent;
+use Symfony\Component\Security\Core\AuthenticationEvents;
 
 class LoginSubscriber implements EventSubscriberInterface
 {
-    private $eventRepository;
-    private $entityManager;
-    private $stateService;
+    private EventRepository $eventRepository;
+    private EntityManager $entityManager;
+    private StateService $stateService;
+    private $registerLimitTime = 'PT24H';
 
     public function __construct(EventRepository $eventRepository, StateService $stateService, EntityManager $entityManager)
     {
@@ -26,23 +28,29 @@ class LoginSubscriber implements EventSubscriberInterface
 
     public function onSecurityAuthenticationSuccess(AuthenticationEvent $authenticationEvent)
     {
+            $now = new \DateTime();
+
         $openEventsToUpload = $this->eventRepository->findEventByStateLabel('Ouverte');
-
         foreach ($openEventsToUpload as $event) {
-            if ($event->getStartDateTime()<=(date_sub($event->getStartDateTime(), new DateInterval('PT24H')))) {
+            dump($event->getStartDateTime());
+            dump(date_sub($event->getStartDateTime(), new DateInterval($this->registerLimitTime)));
+            if ($event->getStartDateTime()<=(date_sub($event->getStartDateTime(), new DateInterval($this->registerLimitTime)))) {
                 $this->stateService->closedState($event);
-
+                dump($event);
                 $this->entityManager->flush();
             }
         }
 
-        $closedEventsToUpload = $this->eventRepository->findEventByStateLabel('Clôturée');
+     dump(   $closedEventsToUpload = $this->eventRepository->findEventByStateLabel('Clôturée'));
         foreach ($closedEventsToUpload as $event) {
             try {
-                $eventFinishingDate = date_add($event->getStartDateTime(), new DateInterval('P' . $event->getDuration() . 'M'));
-
-
-                if ($event->getStartDateTime() <= date("Y/m/d") && $eventFinishingDate >= date("Y/m/d")) {
+                $start = $event->getStartDateTime();
+                $eventFinishingDate = date_add($start, new DateInterval('PT' . $event->getDuration() . 'M'));
+                dump($event->getStartDateTime());
+                dump($eventFinishingDate);
+                dump($now);
+                if (($event->getStartDateTime() <= $now) && ($eventFinishingDate >= $now)) {
+                    dump($event);
                     $this->stateService->inProgressState($event);
 
                     $this->entityManager->flush();
@@ -53,10 +61,10 @@ class LoginSubscriber implements EventSubscriberInterface
         $inProgressEventsToUpload =  $this->eventRepository->findEventByStateLabel('En cours');
         foreach ($inProgressEventsToUpload as $event) {
             try {
-                if (date_add($event->getStartDateTime(), new DateInterval('P' . $event->getDuration() . 'M')) < date("Y/m/d")) {
+                dump(new DateInterval('PT' . $event->getDuration() . 'M'));
+                if (date_add($event->getStartDateTime(), new DateInterval('PT' . $event->getDuration() . 'M')) < $now) {
                     $this->stateService->finishedState($event);
-
-
+                    dump($event);
                     $this->entityManager->flush();
                 }
             } catch (Exception $e) {
@@ -66,11 +74,10 @@ class LoginSubscriber implements EventSubscriberInterface
         $finishedEventsToUpload =  $this->eventRepository->findEventByStateLabel('Terminée');
         foreach ($finishedEventsToUpload as $event) {
             try {
-                $eventFinishingDate = date_add($event->getStartDateTime(), new DateInterval('P' . $event->getDuration() . 'M'));
-
+                $eventFinishingDate = date_add($event->getStartDateTime(), new DateInterval('PT' . $event->getDuration() . 'M'));
                 if (date_diff($event->getStartDateTime(), $eventFinishingDate) >= new DateInterval('P1M')) {
                     $this->stateService->archivedState($event);
-
+                    dump($event);
                     $this->entityManager->flush();
                 }
             } catch (Exception $e) {
@@ -82,7 +89,7 @@ class LoginSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-            'security.authentication.success' => 'onSecurityAuthenticationSuccess',
+            AuthenticationEvents::AUTHENTICATION_SUCCESS => 'onSecurityAuthenticationSuccess',
         ];
     }
 }
