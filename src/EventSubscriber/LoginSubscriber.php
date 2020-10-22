418c06a206b686ad9210098b6343fb5dc81fd522
Kim <68189659+kimmaroe@users.noreply.github.com>
2020-10-22 10:53:14 +0200

    LoginSubscriber.php StateService.php

--- src/EventSubscriber/LoginSubscriber.php
+++ src/EventSubscriber/LoginSubscriber.php
@@ -6,10 +6,9 @@ use App\EventServices\StateService;
 use App\Repository\EventRepository;
 use DateInterval;
 use Doctrine\ORM\EntityManagerInterface as EntityManager;
-use Exception;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Security\Core\Event\AuthenticationEvent;
-use Symfony\Component\Security\Core\AuthenticationEvents;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class LoginSubscriber implements EventSubscriberInterface
 {
@@ -33,7 +32,7 @@ class LoginSubscriber implements EventSubscriberInterface
     }
 
 
-    public function onSecurityAuthenticationSuccess(AuthenticationEvent $authenticationEvent)
+    public function onKernelController(ControllerEvent $event)
     {
             $now = new \DateTime();
             $allEvent = $this->eventRepository->findAll();
@@ -99,69 +98,13 @@ class LoginSubscriber implements EventSubscriberInterface
                 $this->entityManager->flush();
             }
 
-
-
-/*        $openEventsToUpload = $this->eventRepository->findEventByStateLabel('Ouverte');
-        foreach ($openEventsToUpload as $event) {
-            $start = $event->getStartDateTime();
-            dump($event->getStartDateTime());
-            dump(date_sub($start, new DateInterval($this->registerLimitTime)));
-            if ($event->getStartDateTime()<=(date_sub($start, new DateInterval($this->registerLimitTime)))) {
-                $this->stateService->closedState($event);
-                dump($event);
-                $this->entityManager->flush();
-            }
-        }
-
-     dump($closedEventsToUpload = $this->eventRepository->findEventByStateLabel('Clôturée'));
-        foreach ($closedEventsToUpload as $event) {
-            try {
-                $start = $event->getStartDateTime();
-                $eventFinishingDate = date_add($start, new DateInterval('PT' . $event->getDuration() . 'M'));
-                dump($event->getStartDateTime());
-                dump($eventFinishingDate);
-                dump($now);
-                if (($event->getStartDateTime() <= $now) && ($eventFinishingDate >= $now)) {
-                    dump($event);
-                    $this->stateService->inProgressState($event);
-
-                    $this->entityManager->flush();
-                }
-            } catch (Exception $e) {
-            }
-        }
-        $inProgressEventsToUpload =  $this->eventRepository->findEventByStateLabel('En cours');
-        foreach ($inProgressEventsToUpload as $event) {
-            try {
-                dump(new DateInterval('PT' . $event->getDuration() . 'M'));
-                if (date_add($event->getStartDateTime(), new DateInterval('PT' . $event->getDuration() . 'M')) < $now) {
-                    $this->stateService->finishedState($event);
-                    dump($event);
-                    $this->entityManager->flush();
-                }
-            } catch (Exception $e) {
-            }
-        }
-
-        $finishedEventsToUpload =  $this->eventRepository->findEventByStateLabel('Terminée');
-        foreach ($finishedEventsToUpload as $event) {
-            try {
-                $eventFinishingDate = date_add($event->getStartDateTime(), new DateInterval('PT' . $event->getDuration() . 'M'));
-                if (date_diff($event->getStartDateTime(), $eventFinishingDate) >= new DateInterval('P1M')) {
-                    $this->stateService->archivedState($event);
-                    dump($event);
-                    $this->entityManager->flush();
-                }
-            } catch (Exception $e) {
-            }
-        }*/
     }
 
 
     public static function getSubscribedEvents()
     {
         return [
-            AuthenticationEvents::AUTHENTICATION_SUCCESS => 'onSecurityAuthenticationSuccess',
+            KernelEvents::CONTROLLER => 'onKernelController',
         ];
     }
 }
