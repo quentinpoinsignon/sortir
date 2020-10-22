8cea623f413cf318035f72febe21f089536026fc
Kim <68189659+kimmaroe@users.noreply.github.com>
2020-10-22 11:10:02 +0200

    LoginSubscriber.php StateService.php

--- src/EventSubscriber/LoginSubscriber.php
+++ src/EventSubscriber/LoginSubscriber.php
@@ -36,63 +36,61 @@ class LoginSubscriber implements EventSubscriberInterface
     {
             $now = new \DateTime();
             $allEvent = $this->eventRepository->findAll();
-            dump($allEvent);
+
 
             foreach ($allEvent as $event)
             {
-                dump($event);
+
                 $startingDate = $event->getStartDateTime();
                 $startingDateclone = clone $startingDate;
                 $finishingDate = date_add($startingDateclone, new DateInterval('PT'.$event->getDuration().'M'));
-                dump($startingDate);
-                dump($startingDateclone);
-                dump($finishingDate);
+
 
 
                 //récupération des évènements publiés
                 if($event->getState()->getLabel()==self::OPENED_STATE){
-                    dump($event);
+
                     //test si la date de début de l'évènement est dans moins de 24h
                     if((date_diff($now,$startingDateclone)->days)<1)
                     {
                         //appel du stateService pour changement du statut de la sortie si le test est vrai
                         $this->stateService->closedState($event);
-                        dump($event);
+
                     }
 
                 }
                 //récupération des évènements clôturés
                 if($event->getState()->getLabel()==self::CLOSED_STATE)
                 {
-                    dump($event);
+
                     //test si la date de début de l'évènement est antéreure à maintenant et si la date de fin est postérieure à maintenant
                     if($event->getStartDateTime()<=$now && $finishingDate>=$now)
                     {
                         //appel du stateService pour changement du statut de la sortie si le test est vrai
                         $this->stateService->inProgressState($event);
-                        dump($event);
+
                     }
                 }
                 //récupération des évènements en cours
                 if ($event->getState()->getLabel()==self::IN_PROGRESS_STATE){
-                    dump($event);
+
                     //test si la date de fin de l'évènement est antérieur à maintenant
                     if ($finishingDate<$now)
                     {
                         //appel du stateService pour changement du statut de la sortie si le test est vrai
                         $this->stateService->finishedState($event);
-                        dump($event);
+
                     }
                 }
 
                 //récupération des évènements terminés
                 if ($event->getState()->getLabel()==self::FINISHED_STATE)
                 {
-                    dump($event);
+
                     if ((date_diff($now,$event->getStartDateTime())->days)>30)
                     {
                         $this->stateService->archivedState($event);
-                        dump($event);
+
                     }
                 }
                 $this->entityManager->flush();
